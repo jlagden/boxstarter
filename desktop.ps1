8237200b77b23e99bf5dacc7411676a39d19c0f5
jlagden <jlagden@binary.org.uk>
2018-05-15 15:17:48 +0100

    Update desktop.ps1

--- desktop.ps1
+++ desktop.ps1
@@ -2,3 +2,16 @@
 
 #--- Windows Features ---
 Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
+
+#--- Applications ---
+choco install firefox 
+
+#--- Gaming ---
+
+choco install steam
+
+#--- Tools ---
+choco install 7zip.install
+choco install putty.install
+choco install winscp.install
+
