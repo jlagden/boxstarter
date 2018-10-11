28beb24516fb340dd681e7a4a408166af2ebfa3a
jlagden <jlagden@binary.org.uk>
2018-10-11 15:49:55 +0100

    Added script for netbook

---
+++ netbook.ps1
@@ -0,0 +1,33 @@
+# Boxstarter Install Script - Standard Netbook
+
+#--- Windows Features ---
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -EnableShowFullPathInTitleBar
+
+#--- Applications ---
+choco install firefox
+choco install gimp
+
+#--- Gaming ---
+
+#--- Tools ---
+choco install 7zip.install
+choco install putty.install
+choco install winscp.install
+choco install keepassx
+choco install beyondcompare
+choco install sysinternals
+choco install procexp
+choco install foxitreader
+choco install lockhunter
+choco install f.lux
+
+choco install adblockplusie
+choco install mpc-hc
+choco install k-litecodecpackfull
+
+#--- Development ---
+
+choco install git.install
+choco install nodejs.install
+choco install python
+choco install ilspy
