dc3b3947a13f2e3cbefa2fbc01fe92bb45516eb6
jlagden <jlagden@binary.org.uk>
2018-11-12 16:06:48 +0000

    Update desktop.ps1

--- desktop.ps1
+++ desktop.ps1
@@ -8,9 +8,9 @@ Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtec
 choco install firefox
 #choco install gimp # (Image manipulation)
 #choco install inkscape # (Vector graphics editor)
-choco install kodi
+#choco install kodi
 #choco install thunderbird # (Email, Usenet, RSS client)
-choco install virtualbox
+#choco install virtualbox
 
 #--- Gaming ---
 choco install steam
@@ -20,23 +20,23 @@ choco install 7zip.install
 choco install adblockplusie # (Adblock Plus for Edge)
 choco install cmder # (Command Line client)
 choco install deluge # (BitTorrent client)
-choco install duckieTV # (Personalised TV Show Calendar)
+#choco install duckieTV # (Personalised TV Show Calendar)
 choco install foxitreader # (PDF Reader)
 choco install f.lux # (Display adapts to time of day)
 choco install keepassxc # (Password Manager)
 choco install k-litecodecpackfull # (Codec pack for mpc)
-choco install mp3tag # (mp3 metadata)
+#choco install mp3tag # (mp3 metadata)
 choco install mpc-hc # (Video player)
-choco install openvpn # (Virtual Private Network)
-choco install pidgin # (Instant Message Client)
+#choco install openvpn # (Virtual Private Network)
+#choco install pidgin # (Instant Message Client)
 choco install putty.install # (Terminal Emulator)
-choco install signal # (Secure Messaging)
-choco install totalcommander # (File Manager)
-choco install windirstat
+#choco install signal # (Secure Messaging)
+#choco install totalcommander # (File Manager)
+#choco install windirstat
 choco install winscp.install
-choco install wireshark
-choch install xming # (X Server)
-choco install youtube-dl # (Download youtube videos)
+#choco install wireshark
+#choco install xming # (X Server)
+#choco install youtube-dl # (Download youtube videos)
 
 #--- System Tools ---
 
@@ -50,13 +50,13 @@ choco install youtube-dl # (Download youtube videos)
 
 choco install infrarecorder
 choco install rufus
-Choco install win32diskimager.install 
+choco install win32diskimager.install 
 
 #--- Development ---
 
 choco install beyondcompare
 choco install git.install # (Git Source Control)
-choco install ilspy
+#choco install ilspy
 choco install nodejs.install # (Node.js)
 choco install python # (Python programming language)
 #choco install sql-server-express # (SQL Server)
