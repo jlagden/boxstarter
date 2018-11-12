fbb0e7951d91df94c695fe0f4fabe2ea50faca58
jlagden <jlagden@binary.org.uk>
2018-11-12 12:05:16 +0000

    Update desktop.ps1

--- desktop.ps1
+++ desktop.ps1
@@ -4,25 +4,24 @@
 Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -EnableShowFullPathInTitleBar
 
 #--- Applications ---
-choco install firefox
-choco install thunderbird
-choco install inkscape
-choco install gimp
 choco install audacity
+choco install firefox
+choco install gimp
+choco install inkscape
 choco install kodi
+choco install thunderbird
 choco install virtualbox
 
 #--- Gaming ---
 choco install steam
 
-#--- Tools ---
+#--- General Tools ---
 choco install 7zip.install
 choco install putty.install
 choco install winscp.install
 choco install keepassxc
 choco install beyondcompare
 choco install sysinternals
-choco install procexp
 choco install foxitreader
 choco install wireshark
 choco install youtube-dl
@@ -34,6 +33,12 @@ choco install windirstat
 choco install adblockplusie
 choco install mpc-hc
 choco install k-litecodecpackfull
+
+#--- Imaging Tools ---
+
+choco install infrarecorder
+choco install rufus
+Choco install win32diskimager.install 
 
 #--- Development ---
 
